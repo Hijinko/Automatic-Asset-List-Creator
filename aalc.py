b08ffd0bf12ca06b79ec31a4bdac84ac02809cc8
Kevin Ellis <kellis@dgs.home.hijinko.net>
2019-11-21 12:45:39 -0500

    can now find services and mac however there is a bug when parsing ips with no hostname. regex bug

--- aalc.py
+++ aalc.py
@@ -19,15 +19,34 @@ with open(filename, 'r') as f:
     #store each line in a list to later parse
     lines = f.readlines()
 
-#get regex for ip addresses
-ips = re.compile(r'Nmap scan report for (.*) \(+((\d{1,3}\.){3}\d{1,3})\)+')
+#get regex for ip addresses and hostnames
+ips = re.compile(r'Nmap scan report for (.*)|\s \(+((\d{1,3}\.){3}\d{1,3})\)+')
+#get regex to match ports, protocals, and services
+pps = re.compile(r'\d*\/(udp|tcp)\s*(open|closed)\s*(\w|\-)*')
+#get regex to match mac address and device manufacturer
+macs = re.compile(r'MAC Address: (([A-F0-9]{2}:){5}[A-F0-9]{2}) \((.*)\)')
 
-x = 0
-#parse through and find the ips
+x = 0 #place holder for testing delete later
+
+#begin parsing through scan
 for line in lines:
+    #set empty list for ports and protocols
+    hostname = ''
+    host = ''
+    mac_addr = ''
+    #if the line is the beginining of a hosts information
     if (ips.search(line)):
         ip = ips.search(line)
-        print(f"{x}: hostname: {ip.group(1)}\nip: {ip.group(1)}\n")
-        #print(f"{x}: {line[ip.start():ip.end()]}")
+        hostname = ip.group(1)
+        host = ip.group(2)
+    if (pps.search(line)):
+        ports = pps.search(line)
+        print(f'\t{ports.group(0)}')
+    if (macs.search(line)):
+        mac = macs.search(line)
+        mac_addr = mac.group(1)
+        print(f'\tMAC: {mac_addr}\n')
+    if host != '':
         x += 1
-
+        print(f'\n{x}:\thostname: {hostname}\n\tip: {host}')
+        
