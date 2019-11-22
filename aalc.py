35d36b9d6c6b15c10f94efea4a50e1e202b67f81
kellis <kellis@debian.home.hijinko.net>
2019-11-22 00:04:18 -0500

    added more features, still has a bug with the MAC addresses not formatting correctly. Also need to add check for ips that dont have open ports

--- aalc.py
+++ aalc.py
@@ -10,6 +10,7 @@
 ###################################################################################
 
 import re
+import csv
 import sys
 
 #get the filename of the nmap results
@@ -20,33 +21,67 @@ with open(filename, 'r') as f:
     lines = f.readlines()
 
 #get regex for ip addresses and hostnames
-ips = re.compile(r'Nmap scan report for (.*)|\s \(+((\d{1,3}\.){3}\d{1,3})\)+')
+ips = re.compile(r'Nmap scan report for (.*)|\(+((\d{1,3}\.){3}\d{1,3})\)+')
 #get regex to match ports, protocals, and services
 pps = re.compile(r'\d*\/(udp|tcp)\s*(open|closed)\s*(\w|\-)*')
 #get regex to match mac address and device manufacturer
 macs = re.compile(r'MAC Address: (([A-F0-9]{2}:){5}[A-F0-9]{2}) \((.*)\)')
 
 x = 0 #place holder for testing delete later
+header = ("Host Name", 
+          "OS Version",
+          "IP Address",
+          "MAC Address",
+          "Ports Open",
+          "Web Address",
+          "Purpose of Machine",
+          "Critical Services",
+          "Abnormal Service",
+          "Users",
+          "Group e.g. 'Administrators'",
+          "Abnormal Applications",
+          "Abnormal Processes")
 
+#open csv file to begin writing to
+with open('Asset_List.csv', 'w') as asset_list:
+    al_writer = csv.writer(asset_list)
+    #write the coulumn names in the csv file
+    al_writer.writerow(header)
+    al_writer = csv.writer(asset_list)
+
+mac_addr = ''
+ports_list = []
 #begin parsing through scan
 for line in lines:
+    line = line.strip()
     #set empty list for ports and protocols
     hostname = ''
     host = ''
-    mac_addr = ''
+    #mac_addr = ''
     #if the line is the beginining of a hosts information
-    if (ips.search(line)):
-        ip = ips.search(line)
-        hostname = ip.group(1)
-        host = ip.group(2)
-    if (pps.search(line)):
+    #if (ips.search(line)):
+    if "Nmap scan report for" in line:
+        if '(' in line:
+            hostname = line.split(' ')[4]
+            hostname = hostname.rstrip(")")
+            host = line.split(' ')[5]
+        else:
+            hostname = 'n/a'
+            host = line.split(' ')[4]
+    elif (pps.search(line)):
         ports = pps.search(line)
-        print(f'\t{ports.group(0)}')
-    if (macs.search(line)):
+        ports_list.append(ports.group(0))
+        #print(f'\t{ports.group(0)}')
+    elif (macs.search(line)):
         mac = macs.search(line)
         mac_addr = mac.group(1)
-        print(f'\tMAC: {mac_addr}\n')
+        #print(f'\tMAC: {mac_addr}')
     if host != '':
-        x += 1
-        print(f'\n{x}:\thostname: {hostname}\n\tip: {host}')
-        
+        #row = f'{hostname}, "", "", host, "", mac_addr, "", "Ports Open", "", "", "", "", "", "", "", ""' 
+        ports_to_list = '\n'.join(ports_list)
+        print(ports_to_list)
+        with open('Asset_List.csv', 'a') as asset_list:
+            al_writer = csv.writer(asset_list)
+            al_writer.writerow([hostname, "", host, mac_addr, ports_to_list, "", "", "", "", "", "", "", ""]) 
+        mac_addr = ''
+        ports_list = []
